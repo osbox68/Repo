eb101c925b6f74f5d40133ba3727edf54c4338f3
Luis Munoz <osbox68@gmail.com>
2021-06-30 10:53:01 -0400

    Sort OK

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,30 @@ require_relative "wagon_sort"
 #       - `gets`:  http://www.ruby-doc.org/core-2.5.3/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
 
+students  = []
+name      = nil
 
+while name != ""
+  puts students.empty? ? 'Ingrese Nombre de un Estudiante:' : 'Ingrese otro Nombre (o de enter para terminar):'
+  name = gets.chomp
 
-# TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
-#       file and display the sorted student list
+  students << name if name != ""
+end
+
+sorted_students = wagon_sort(students)
+num_students = sorted_students.size
+
+def pluralize(nin, word)
+  if nin == 1
+      "#{word}"
+  else
+      "#{word}s"
+  end
+end
+
+puts "Excelente le WAgon tiene #{num_students} #{pluralize(num_students, "student")}:"
+if sorted_students.size >= 2
+  puts "#{sorted_students[0..-2].join(', ')} and #{sorted_students.last}"
+else
+  puts sorted_students.first
+end
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,3 @@
 def wagon_sort(students)
-  # TODO: return (not print!) a copy of students, sorted alphabetically
+  students.sort_by { |student| student.downcase }
 end
