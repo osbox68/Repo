e942fac6d2b6e3d6c177efee747085d733c71139
Luis Munoz <osbox68@gmail.com>
2021-07-01 13:29:41 -0400

    Black Jack Ok

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -1,9 +1,19 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
+  "Su score es #{player_score}, bank is #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
-  # TODO: return (not print!) a message telling if the user won or lost.
+  if player_score > 21
+    "Ud. esta sobre 21... Ud. Pierde!"
+  elsif player_score == 21
+    "Black Jack!"
+  elsif player_score == bank_score
+    "Empate!"
+  elsif player_score > bank_score
+    "Ud. Gana el banco pierde!"
+  else
+    "El BAnco Gana UD. Pierde"
+  end
 end
--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -1,4 +1,30 @@
 require_relative "croupier"
 
-# TODO: make the user play from terminal in a while loop that will stop
-#       when the user will not be asking for  a new card
+puts "*** Bienvenido a Nuestro Divertido Black-Jack ***"
+
+# toma el resultado del banco y lo guarda
+bank_score = pick_bank_score
+# El Jugador se inicializa en 0
+player_score = 0
+running = true
+
+while running
+  # Pregunta si el jugador desea una nueva carta
+  puts "Carta? 'si' or 'no' para conseguir una nueva carta"
+  # tomar la respuesta del usuario y guardarla en una variable
+  player_answer = gets.chomp
+  # comprobar si la respuesta es  's' or 'si':
+  if %w[s si].include?(player_answer)
+    # tomar la carta y sumarla al score del jugador
+    player_score += pick_player_card
+    # Mostrar el estado del Juego
+    puts state_of_the_game(player_score, bank_score)
+    running = player_score < 21
+  else
+    running = false
+  end
+  # retornar por una nueva carta
+end
+# Mensaje final sobre el estado del Juego
+puts end_game_message(player_score, bank_score)
+# cuando finalice el jugador no sera consultado por una nueva carta
